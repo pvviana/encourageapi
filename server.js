f7e919a78f15f2148d0927a1b0903b5f57e13e91
SINAF\pviana <paulovictorviana@globomail.com>
2018-03-20 15:39:40 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/encourageModel'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/encourageRoutes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+  });
+
+
+console.log('EncourageApp inciado na porta: ' + port);
