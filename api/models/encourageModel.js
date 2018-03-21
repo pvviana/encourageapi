f9e2484317b714108aec57bb8739c6b34f89b9a5
SINAF\pviana <paulovictorviana@globomail.com>
2018-03-21 09:23:17 -0300

    Inclusão de senha hash no modelo Perfil

--- api/models/encourageModel.js
+++ api/models/encourageModel.js
@@ -1,12 +1,17 @@
 'use strict';
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
 var PerfilSchema = new Schema({
   nome: {
     type: String,
     required: 'Escreva seu Nome.'
+  },
+  senha: { 
+    type: String, 
+    required: true 
   },
   sexo:{
         type: String,
@@ -21,17 +26,55 @@ var PerfilSchema = new Schema({
       type: Number, 
       required: 'Escreva seu cpf.'
     },
-  Created_date: {
-    type: Date,
-    default: Date.now
-  },
   status: {
     type: [{
       type: String,
       enum: ['Normal', 'Moderador', 'Administrador']
     }],
     default: ['Normal']
+  },
+  Usuario_Confirmado: {
+    type: [{
+      type: String,
+      enum: ['N', 'S']
+    }],
+    default: ['N']
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
   }
 });
+
+PerfilSchema.pre('save', function(next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('senha')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+      if (err) return next(err);
+
+      // hash the password using our new salt
+      bcrypt.hash(user.senha, salt, function(err, hash) {
+          if (err) return next(err);
+
+          // override the cleartext password with the hashed one
+          user.senha = hash;
+          next();
+      });
+  });
+});
+
+PerfilSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.senha, function(err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+  });
+};
+
+
+
 
 module.exports = mongoose.model('Perfils', PerfilSchema);
